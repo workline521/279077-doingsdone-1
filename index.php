fd9d4d4d50a6c063dfaf0961a5db8af782c56fb2
Evgenii Isupov <workline521@gmail.com>
2017-08-31 21:49:52 +0400

    #5 Арифметика и работа со временем

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ $task_deadline_ts = strtotime("+" . $days . " day midnight"); // метка вр
 $current_ts = strtotime('now midnight'); // текущая метка времени
 
 // запишите сюда дату выполнения задачи в формате дд.мм.гггг
-$date_deadline = null;
+$date_deadline = date("d.m.Y", $task_deadline_ts);
 
 // в эту переменную запишите кол-во дней до даты задачи
-$days_until_deadline = null;
+$days_until_deadline = floor(($task_deadline_ts - $current_ts) / 86400);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -144,7 +144,7 @@ $days_until_deadline = null;
                         </td>
                     </tr>
                     <?php endif; ?>
-                    <tr class="tasks__item task">
+                    <tr class="tasks__item task <?= ($days_until_deadline <= 0) ?  'task--important' : '' ?>">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
                                 <input class="checkbox__input visually-hidden" type="checkbox">
@@ -154,6 +154,7 @@ $days_until_deadline = null;
 
                         <td class="task__date">
                             <!--выведите здесь дату выполнения задачи-->
+                            <?= $date_deadline ?>
                         </td>
 
                         <td class="task__controls">
